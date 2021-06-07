234f0364231f35fdcd64e61afda1fed70eefec59
Barry Weeks <bweeks101@gmail.com>
2021-06-07 16:07:13 +0100

    Fixed linting errors in project level settings.py

--- dice_lounge/settings.py
+++ dice_lounge/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qpk&r13_%-x5a1snqc_&b1xvj9o7u1yl@p-^=gz-u%-0zhs4sa'
+s_key_prefix = 'django-insecure-'
+s_key_suffix = 'qpk&r13_%-x5a1snqc_&b1xvj9o7u1yl@p-^=gz-u%-0zhs4sa'
+SECRET_KEY = s_key_prefix + s_key_suffix
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,6 +44,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    # dice_lounge apps
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +64,10 @@ ROOT_URLCONF = 'dice_lounge.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,18 +120,20 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
+pword_validator_str = 'django.contrib.auth.password_validation.'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': pword_validator_str + 'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': pword_validator_str + 'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': pword_validator_str + 'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': pword_validator_str + 'NumericPasswordValidator',
     },
 ]
 
