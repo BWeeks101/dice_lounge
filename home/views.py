6fc583c4693e4f1a6fd07ec656634592f6eaf413
Barry Weeks <bweeks101@gmail.com>
2021-07-01 09:53:03 +0100

    Added all_games url to template and search redirect to view

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def index(request):
-    """ A view to return the index page """
+    """
+        A view to return the index page or redirect a search
+    """
+
+    if request.GET:
+        if 'q' in request.GET:
+            return redirect('/products/search_results/?q=' + request.GET['q'])
+
     return render(request, 'home/index.html')
