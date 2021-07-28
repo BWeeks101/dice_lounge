3423d10ee283c8fc51e44e1e6fa66fb4bf40d9a7
Barry Weeks <bweeks101@gmail.com>
2021-07-28 16:45:50 +0100

    Added dj_database_url config

--- dice_lounge/settings.py
+++ dice_lounge/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 if os.path.exists("env.py"):
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['the-dice-lounge.herokuapp.com', 'localhost']
 
 if DEBUG is True:
     ALLOWED_HOSTS.append(os.environ.get("development_host"))
@@ -143,12 +144,17 @@ WSGI_APPLICATION = 'dice_lounge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
