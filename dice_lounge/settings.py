368eec94433f59d75e2744723c8404812228c014
Barry Weeks <bweeks101@gmail.com>
2021-07-28 18:15:37 +0100

    Added boto3, django-storages, AWS config

--- dice_lounge/settings.py
+++ dice_lounge/settings.py
@@ -59,6 +59,9 @@ INSTALLED_APPS = [
     # django-countries
     'django_countries',
 
+    # django-storages
+    'storages',
+
     # dice_lounge apps
     'home',
     'products',
@@ -218,6 +221,31 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 DEFAULT_MAX_PER_PURCHASE = 10
 MAX_PERCENTAGE_REDUCTION = 75
 DEFAULT_PERCENTAGE_REDUCTION = 5
+
+# AWS
+if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'the-dice-lounge'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
